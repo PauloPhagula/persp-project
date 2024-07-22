5e4725d850caff9e6a3309a4b60fe1d70184a7ed
Paulo Phagula <PauloPhagula@users.noreply.github.com>
2024-07-22 12:26:38 +0200

    Initial commit and hopefully last

---
+++ README.md
@@ -0,0 +1,95 @@
+[![License GPL 3][badge-license]](http://www.gnu.org/licenses/gpl-3.0.txt)
+<!-- [![MELPA](http://melpa.org/packages/persp-project-badge.svg)](http://melpa.org/#/persp-project)
+[![MELPA Stable](http://stable.melpa.org/packages/persp-project-badge.svg)](http://stable.melpa.org/#/persp-project) -->
+
+## Introduction
+
+[Perspective](https://github.com/nex3/perspective-el) is a minor mode
+that provides the ability to manage different workspaces. If you need
+to open many projects at the same time, perspective can help you keep
+each project related buffers and windows setting separate from other
+projects, similar to multiple spaces on MacOS, which allows you to
+focus on the files of the current active project.
+
+This library bridges perspective mode to the built-in project library.
+The idea is to create a separate perspective when switching projects.
+A perspective is an independent workspace for Emacs, similar to multiple
+desktops in Gnome and MacOS. This integration allows you to easily know
+which project you're currently in, and focus on files that only belong
+to the current project when switching buffers.
+
+> [!NOTE]  
+> This package has come to life adapting the great package [persp-projectile by Bozhidar Batsov](https://github.com/bbatsov/persp-projectile) with ChatGTP.
+> All credit goes to him ... I have been using his package for years now.
+> What inspired me to create this package is my recent effort to avoid most third party packages, specially if they prefer a newly defined interface over the built-in Emacs interface
+
+<!-- 
+A picture says a thousand words. See below screenshot to get a concrete idea.
+
+Only current project related files showing in minibuffer when I call
+`ido-switch-buffer`, and an indicator in mode line tells me which
+project that I'm in.
+
+![persp-project Screenshot 1](screenshots/persp-project1.png)
+
+When I switch to a different project, I get a clean 'perspective'.
+
+![persp-project Screenshot 2](screenshots/persp-project2.png) -->
+
+## Installation
+
+I'll assume that if you want to use this package you're already using perspective and have set it up already.
+If you're not, you can find the install instructions in the project's README.
+
+This package is not yet available on MELPA, so you can either install it manually or by combining use-package with straight.el or quelpa.
+
+## Manually
+
+Download the `persp-project.el` file and add it to your load path.
+Most likely you will want to add it to your `~/.emacs.d/lisp` directory.
+
+And then request it via use-package more or less like so:
+
+```elisp
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(use-package persp-project
+  :load-path "lisp/persp-project"
+  :after (perspective project))
+```
+
+## Automatically through straight.el or quelpa
+
+If you're using straight.el, you can add the following to your init.el:
+
+```elisp
+(use-package persp-project
+  :straight (persp-project :type git :host github :repo "PauloPhagula/persp-project")
+  :after (perspective project))
+```
+
+## Known issues
+
+Check out the project's
+[issue list](https://github.com/PauloPhagula/persp-project/issues?sort=created&direction=desc&state=open)
+a list of unresolved issues. By the way - feel free to fix any of them
+and send me a pull request. :-)
+
+## Credits / Thanks
+
+- Bozhidar Batsov ([bbatsov]((https://github.com/bbatsov)) and contributors of the original [persp-projectile](https://github.com/bbatsov/persp-projectile) package.
+- Natalie Weizenbaum ([nex3](https://github.com/nex3)) and contributors of the original [perspective](https://github.com/nex3/perspective-el) package.
+- ChatGTP for helping me adapt Bozhidar's code to use the Emacs built-in project.el.
+
+## Contributors
+
+Here's a [list](https://github.com/PauloPhagula/persp-project/contributors) of all the people who have contributed to the
+development of persp-project.
+
+## License
+
+Copyright © 2024 Paulo Phagula and [contributors](https://github.com/PauloPhagula/persp-project/contributors).
+
+Distributed under the GNU General Public License, version 3
+
+[badge-license]: https://img.shields.io/badge/license-GPL_3-green.svg
---
+++ persp-project.el
@@ -0,0 +1,104 @@
+;;; persp-project.el --- Perspective integration with built-in project
+
+;; Copyright (C) 2024 Paulo Phagula
+
+;; Author: Paulo Phagula
+;; Created: 2024-07-07
+;; Keywords: project, convenience
+;; Version: 1.0.0
+;; Package-Requires: ((perspective "1.9") (project "0.6.1") (cl-lib "0.3"))
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;
+;; This library bridges perspective mode to the built-in project library.
+;; The idea is to create a separate perspective when switching projects.
+;; A perspective is an independent workspace for Emacs, similar to multiple
+;; desktops in Gnome and MacOS. This integration allows you to easily know
+;; which project you're currently in, and focus on files that only belong
+;; to the current project when switching buffers.
+
+;; To use this library, put this file in your Emacs load path, and
+;; call (require 'persp-project)
+
+;; See perspective.el on github: https://github.com/nex3/perspective-el
+
+;;; Code:
+(require 'perspective)
+(require 'project)
+
+(defmacro project-persp-bridge (func-name)
+  "Create advice to create a perspective before invoking function FUNC-NAME.
+The advice provides a bridge between perspective and project
+functions when switching between projects. After switching to a new
+project, this advice creates a new perspective for that project."
+  `(defadvice ,func-name (before project-create-perspective-after-switching-projects activate)
+     "Create a dedicated perspective for the current project's window after switching projects."
+     (let ((project-name (file-name-nondirectory (directory-file-name (project-root (project-current))))))
+       (when (and persp-mode (project-current))
+         (persp-switch project-name)))))
+
+(project-persp-bridge project-dired)
+(project-persp-bridge project-find-file)
+
+;;;###autoload
+(defun project-persp-switch-project (project-to-switch)
+  "Switch to a project or perspective we have visited before.
+If the perspective of the corresponding project does not exist, this
+function will call `persp-switch' to create one and switch to
+that before `project-switch-project' invokes
+`project-switch-project-action'.
+
+Otherwise, this function calls `persp-switch' to switch to an
+existing perspective of the project unless we're already in that
+perspective."
+  (interactive (list (completing-read "Switch to project: " (project-known-project-roots))))
+  (let* ((project-root (file-name-as-directory project-to-switch))
+         (project-name (file-name-nondirectory (directory-file-name project-root)))
+         (persp (gethash project-name (perspectives-hash))))
+    (cond
+     ;; project-specific perspective already exists
+     ((and persp (not (equal persp (persp-curr))))
+      (persp-switch project-name))
+     ;; persp exists but does not match with project-name
+     ((and persp (not (equal persp project-name)))
+      (persp-switch project-name)
+      (project-switch-project project-root))
+     ;; project-specific perspective doesn't exist
+     ((not persp)
+      (let ((frame (selected-frame)))
+        (persp-switch project-name)
+        (project-switch-project project-root)
+        ;; Clean up if we switched to a new frame. `helm' for one allows finding
+        ;; files in new frames so this is a real possibility.
+        (when (not (equal frame (selected-frame)))
+          (with-selected-frame frame
+            (persp-kill project-name))))))))
+
+(defadvice persp-init-frame (after project-persp-init-frame activate)
+  "Rename initial perspective to `project-name' when a
+new frame is created in a known project."
+  (with-selected-frame frame
+    (when (project-current)
+      (persp-rename (file-name-nondirectory (directory-file-name (project-root (project-current))))))))
+
+(provide 'persp-project)
+;;; persp-project.el ends here
