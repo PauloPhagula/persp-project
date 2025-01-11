7a14bbb833d5b2a95489ae0f262df9c24c1f2c0f
Paulo Phagula <PauloPhagula@users.noreply.github.com>
2025-01-11 12:41:39 +0200

    Improve formatting

--- persp-project.el
+++ persp-project.el
@@ -44,7 +44,7 @@
   (with-selected-frame frame
     (when (and persp-mode (project-current))
       (let ((project-name (file-name-nondirectory
-                           (directory-file-name (project-root (project-current))))))
+                            (directory-file-name (project-root (project-current))))))
         (persp-rename project-name)))))
 
 (defun persp-project--rename-window ()
@@ -68,26 +68,26 @@ perspective.
 PROJECT-TO-SWITCH denotes the project/perspective."
   (interactive (list (completing-read "Switch to project: " (project-known-project-roots))))
   (let* ((project-root (file-name-as-directory project-to-switch))
-         (project-name (file-name-nondirectory (directory-file-name project-root)))
-         (persp (gethash project-name (perspectives-hash))))
+          (project-name (file-name-nondirectory (directory-file-name project-root)))
+          (persp (gethash project-name (perspectives-hash))))
     (cond
-     ;; Project-specific perspective already exists
-     ((and persp (not (equal persp (persp-curr))))
-      (persp-switch project-name))
-     ;; Perspective exists but does not match with project-name
-     ((and persp (not (equal persp project-name)))
-      (persp-switch project-name)
-      (project-switch-project project-root))
-     ;; Project-specific perspective doesn't exist
-     ((not persp)
-      (let ((frame (selected-frame)))
+      ;; Project-specific perspective already exists
+      ((and persp (not (equal persp (persp-curr))))
+        (persp-switch project-name))
+      ;; Perspective exists but does not match with project-name
+      ((and persp (not (equal persp project-name)))
         (persp-switch project-name)
-        (project-switch-project project-root)
-        ;; Clean up if we switched to a new frame. `helm` for one allows finding
-        ;; files in new frames so this is a real possibility.
-        (when (not (equal frame (selected-frame)))
-          (with-selected-frame frame
-            (persp-kill project-name))))))))
+        (project-switch-project project-root))
+      ;; Project-specific perspective doesn't exist
+      ((not persp)
+        (let ((frame (selected-frame)))
+          (persp-switch project-name)
+          (project-switch-project project-root)
+          ;; Clean up if we switched to a new frame. `helm` for one allows finding
+          ;; files in new frames so this is a real possibility.
+          (when (not (equal frame (selected-frame)))
+            (with-selected-frame frame
+              (persp-kill project-name))))))))
 
 
 ;;;###autoload
@@ -100,21 +100,19 @@ Creates perspective for projects."
   :global t
 
   (if persp-project-mode
-      (if (and persp-mode (featurep 'project))
-          (progn
-            (advice-add 'project-dired :before #'persp-project--rename-window)
-            (advice-add 'project-find-file :before #'persp-project--rename-window)
-            (advice-add 'persp-init-frame :after #'persp-project--rename-frame)
-            )
-        (message "You cannot enable persp-project-mode unless persp-mode and project are active.")
-        (setq persp-project-mode nil))
+    (if (and persp-mode (featurep 'project))
+      (progn
+        (advice-add 'project-dired :before #'persp-project--rename-window)
+        (advice-add 'project-find-file :before #'persp-project--rename-window)
+        (advice-add 'persp-init-frame :after #'persp-project--rename-frame))
+      (message "You cannot enable persp-project-mode unless persp-mode and project are active.")
+      (setq persp-project-mode nil))
 
     ;; Disable advices when the mode is turned off
     (progn
-        (advice-remove 'project-dired #'persp-project--rename-window)
-        (advice-remove 'project-find-file #'persp-project--rename-window)
-        (advice-remove 'persp-init-frame #'persp-project--rename-frame)
-      )))
+      (advice-remove 'project-dired #'persp-project--rename-window)
+      (advice-remove 'project-find-file #'persp-project--rename-window)
+      (advice-remove 'persp-init-frame #'persp-project--rename-frame))))
 
 (provide 'persp-project)
 ;;; persp-project.el ends here
